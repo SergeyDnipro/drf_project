f29540a95a1e5cdd28cfd56a71e08cc3361ce0eb
Botte <votetob@gmail.com>
2022-05-31 21:19:24 +0300

    ideas

--- drf_fuel_project/fuel_search/serializers/StationSupplySerializer.py
+++ drf_fuel_project/fuel_search/serializers/StationSupplySerializer.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 from fuel_search.models.gas_station import Supply
+from decimal import Decimal
 
 
 class StationSupplySerializer(serializers.ModelSerializer):
     fuel = serializers.CharField(source='fuel.type')
+
+    price_in_uah = serializers.SerializerMethodField()
+
+    def get_price_in_uah(self, instance):
+        return instance.price * Decimal(29.25)
 
     class Meta:
         model = Supply
@@ -11,4 +17,5 @@ class StationSupplySerializer(serializers.ModelSerializer):
             'gas_station',
             'fuel',
             'price',
+            'price_in_uah',
         )
